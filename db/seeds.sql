998a9c9f5fcf306ec33e8493397360ff50c67f5e
DimitriNakos <dimi1nakos@gmail.com>
2020-12-12 17:05:14 -0500

    fixed input to not allow hacking from Jason

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO games (name) VALUES ('Lufia 2');
-INSERT INTO games (name, completed) VALUES ('Final Fantasy 3', true);
-INSERT INTO games (name, completed) VALUES ('Chrono Trigger', true);
+INSERT INTO games (name, completed) VALUES ('Final Fantasy 3');
+INSERT INTO games (name, completed) VALUES ('Chrono Trigger');
