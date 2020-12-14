fb978e69f0ba465503fed5ed52359167c5d60577
DimitriNakos <dimi1nakos@gmail.com>
2020-12-13 23:51:49 -0500

    fixed seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 /* Initial values for the table */
 INSERT INTO games (name) VALUES ('Lufia 2');
-INSERT INTO games (name, completed) VALUES ('Final Fantasy 3');
-INSERT INTO games (name, completed) VALUES ('Chrono Trigger');
+INSERT INTO games (name) VALUES ('Final Fantasy 3');
+INSERT INTO games (name) VALUES ('Chrono Trigger');
